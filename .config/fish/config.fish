1c967400df42254d02f3a22501a3b1f20444c3c4
Danielle Lancashire <dani@builds.terrible.systems>
2015-05-02 19:09:56 +0100

    dotfiles stuff

---
+++ .config/fish/config.fish
@@ -0,0 +1,11 @@
+set fish_greeting
+set -x EDITOR vim
+set -x LSCOLORS Gxfxcxdxbxegedabagacad
+
+alias home="git --work-tree=$HOME --git-dir=$HOME/.files.git"
+alias xcp="open *.xcodeproj"
+alias xcw="open *.xcworkspace"
+alias gs="git s"
+alias ga="git add"
+alias gc="git commit"
+alias l="ls -lah"
