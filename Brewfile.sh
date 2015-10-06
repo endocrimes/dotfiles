510c921372c1c35a04513c52910d73e257d090b5
Danielle Lancashire <dani@builds.terrible.systems>
2015-10-06 16:45:18 +0100

    Update brewfile

--- Brewfile.sh
+++ Brewfile.sh
@@ -2,36 +2,43 @@
 brew install caskroom/cask/brew-cask;
 brew tap caskroom/fonts
 
-brew install zsh;
-brew install fish;
-brew install vim;
-brew install tree;
-brew install wget;
-brew install ffmpeg;
-brew install python;
-brew install python3;
-brew install rbenv;
-brew install ghc;
-brew install cabal-install;
-brew install git;
-brew install hub;
-brew install heroku-toolbelt;
-brew install tmux;
-brew install class-dump;
-brew install npm;
-brew install node;
+function install_brew() {
+	brew install $1	
+}
 
-brew cask install font-inconsolata;
-brew cask install font-droid-sans;
-brew cask install font-fontawesome;
+function install_cask() {
+	brew cask install $1
+}
 
-brew cask install iterm2;
-brew cask install audio-hijack;
-brew cask install alfred;
-brew cask install airdisplay;
-brew cask install slack;
-brew cask install hopper-disassembler;
-brew cask install reveal;
-brew cask install arq;
-brew cask install charles;
-brew cask install tower;
+install_brew zsh;
+install_brew fish;
+install_brew vim;
+install_brew tree;
+install_brew wget;
+install_brew ffmpeg;
+install_brew python;
+install_brew python3;
+install_brew rbenv;
+install_brew ghc;
+install_brew cabal-install;
+install_brew git;
+install_brew hub;
+install_brew heroku-toolbelt;
+install_brew tmux;
+install_brew class-dump;
+install_brew npm;
+install_brew node;
+
+install_cask font-inconsolata;
+install_cask font-droid-sans;
+install_cask font-fontawesome;
+install_cask iterm2;
+install_cask audio-hijack;
+install_cask alfred;
+install_cask airdisplay;
+install_cask slack;
+install_cask hopper-disassembler;
+install_cask reveal;
+install_cask arq;
+install_cask charles;
+install_cask tower;
