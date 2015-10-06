7708d7b30702db0cf6a9fe94692b0c9961f014b5
Danielle Lancashire <dani@builds.terrible.systems>
2015-10-06 20:00:30 +0100

    Add bin to user paths

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ set fish_greeting
 set -x EDITOR vim
 set -x LSCOLORS Gxfxcxdxbxegedabagacad
 
+set -U fish_user_paths ~/bin
+
 alias home="git --work-tree=$HOME --git-dir=$HOME/.files.git"
 alias xcp="open *.xcodeproj"
 alias xcw="open *.xcworkspace"
