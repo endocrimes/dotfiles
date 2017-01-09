d7330df84d2e17014f69fe38c0529f2634921800
Danielle Tomlinson <dan@tomlinson.io>
2017-01-09 16:31:04 -0500

    Fixes for ubuntu

--- .config/os/ubuntu/install.sh
+++ .config/os/ubuntu/install.sh
@@ -53,16 +53,49 @@ install_package "Tmux" "tmux"
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-install_package "New Sources" "apt-transport-https"
-install_package "New Certificates" "ca-certificates"
+install_package "Terminator" "terminator"
 
-sudo apt-key adv \
-  --keyserver hkp://ha.pool.sks-keyservers.net:80 \
-  --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-add_to_source_list "https://apt.dockerproject.org/repo ubuntu-xenial main" "docker.list"
+install_package "exfat-fuse" "exfat-fuse"
 
-execute "apt-get update"
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-install_package "Docker" "docker-engine"
+install_package "exfat-utils" "exfat-utils"
 
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_package "notmuch" "notmuch"
+install_package "alot" "alot"
+install_package "msmtp" "msmtp"
+install_package "getmail" "getmail"
+install_package "w3c" "w3c"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_package "GnuPG2" "gnupg2"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_package "Node" "node"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+install_package "NPM" "npm"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+if ! package_is_installed "docker-engine"; then
+  install_package "New Sources" "apt-transport-https"
+  install_package "New Certificates" "ca-certificates"
+
+  sudo apt-key adv \
+    --keyserver hkp://ha.pool.sks-keyservers.net:80 \
+    --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+  add_to_source_list "https://apt.dockerproject.org/repo ubuntu-xenial main" "docker.list"
+
+  execute "sudo apt-get update"
+
+  install_package "Docker" "docker-engine"
+fi
--- .zshrc
+++ .zshrc
@@ -165,10 +165,12 @@ export PATH="~/bin:$PATH"
 
 ## GPG
 
-if test -f ~/.gnupg/.gpg-agent-info -a -n "$(pgrep gpg-agent)"; then
-  source ~/.gnupg/.gpg-agent-info
-  export GPG_AGENT_INFO
-else
-  eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+if [[ $(uname -a) == "Darwin" ]]; then
+  if test -f ~/.gnupg/.gpg-agent-info -a -n "$(pgrep gpg-agent)"; then
+    source ~/.gnupg/.gpg-agent-info
+    export GPG_AGENT_INFO
+  else
+    eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+  fi
 fi
 
