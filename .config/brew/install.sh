e9a49d3c4b4aff47cce7eaee3fd25bca3acaff21
Danielle Tomlinson <dan@tomlinson.io>
2017-01-09 09:20:05 -0500

    [Brews] Add mail brews

--- .config/brew/install.sh
+++ .config/brew/install.sh
@@ -1,15 +1,16 @@
-#! /bin/bash
+#!/usr/bin/env bash
+
 brew install caskroom/cask/brew-cask;
 brew tap caskroom/fonts;
-brew tap danieltomlinson/formulae;
+brew tap dantoml/formulae;
 brew tap kylef/formulae;
 
 function install_brew() {
-	brew install $1;
+	brew install "$1";
 }
 
 function install_cask() {
-	brew cask install $1;
+	brew cask install "$1";
 }
 
 install_brew zsh;
@@ -22,6 +23,7 @@ install_brew python;
 install_brew python3;
 install_brew rbenv;
 install_brew ghc;
+install_brew gnupg2;
 install_brew cabal-install;
 install_brew git;
 install_brew hub;
@@ -31,14 +33,18 @@ install_brew class-dump;
 install_brew npm;
 install_brew node;
 install_brew shellcheck;
+install_brew notmuch;
+install_brew alot;
+install_brew msmtp;
+install_brew getmail;
+install_brew w3c;
 
 install_brew kylef/formulae/swiftenv;
 
-install_brew danieltomlinson/formulae/git-cleanup;
-install_brew danieltomlinson/formulae/git-promote;
+install_brew dantoml/formulae/git-cleanup;
+install_brew dantoml/formulae/git-promote;
 
 install_cask font-inconsolata;
-install_cask font-droid-sans;
 install_cask font-fontawesome;
 install_cask iterm2;
 install_cask audio-hijack;
@@ -47,10 +53,8 @@ install_cask airdisplay;
 install_cask slack;
 install_cask hopper-disassembler;
 install_cask reveal;
-install_cask arq;
 install_cask charles;
 install_cask tower;
 install_cask conductor;
 install_cask google-chrome;
 install_cask quickradar;
-install_cask tripmode;
