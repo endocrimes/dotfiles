1c0aeecab97ede37e5122c059547bf710d4da5b7
Danielle Tomlinson <dan@tomlinson.io>
2017-10-04 11:47:20 +0200

    Re-add zsh history config

---
+++ .config/zsh/history.zsh
@@ -0,0 +1,11 @@
+HISTFILE=~/.zsh_history         # where to store zsh config
+HISTSIZE=1024                   # big history
+SAVEHIST=1024                   # big history
+setopt append_history           # append
+setopt hist_ignore_all_dups     # no duplicate
+unsetopt hist_ignore_space      # ignore space prefixed commands
+setopt hist_reduce_blanks       # trim blanks
+setopt hist_verify              # show before executing history commands
+setopt inc_append_history       # add commands as they are typed, don't wait until shell exit
+setopt share_history            # share hist between sessions
+setopt bang_hist                # !keyword
