357ac774a6a27df503a6d959c8851a2079ce8eeb
Danielle Tomlinson <dani@tomlinson.io>
2018-12-29 16:59:43 +0100

    email: Use keychain for email credentials

--- .config/mutt/credentials/imappwd.py
+++ .config/mutt/credentials/imappwd.py
@@ -1,10 +1,12 @@
-import subprocess
+import re, commands
+def get_keychain_pass(account=None, server=None):
+    params = {
+        'security': '/usr/bin/security',
+        'command':  'find-internet-password',
+        'account':  account,
+        'server':   server
+    }
 
-
-def mailpasswd(acct):
-    path = "email.%s.password" % (acct)
-    args = ["pony", "get", path]
-    try:
-        return subprocess.check_output(args).strip()
-    except subprocess.CalledProcessError:
-        return ""
+    command = "%(security)s %(command)s -g -a %(account)s -s %(server)s" % params
+    outtext = commands.getoutput(command)
+    return re.match(r'password: "(.*)"', outtext).group(1)
