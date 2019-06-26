51846dd09c20f7910b26f94d13d7646998b015ff
Danielle Lancashire <dani@builds.terrible.systems>
2019-06-26 14:26:40 +0200

    mutt: Add support for getting credentials on linux

--- .config/mutt/credentials/imappwd.py
+++ .config/mutt/credentials/imappwd.py
@@ -1,5 +1,18 @@
-import re, commands
-def get_keychain_pass(account=None, server=None):
+import re, commands, subprocess
+from sys import platform
+from os.path import expanduser
+
+def get_keychain_pass_linux(account=None, server=None):
+    params = {
+        'account': account,
+        'home': expanduser("~")
+    }
+
+    command = ["gpg", "--for-your-eyes-only", "-q", "--no-tty", "-d",
+               "%(home)s/.config/mutt/credentials/%(account)s.gpg" % params]
+    return subprocess.check_output(command)
+
+def get_keychain_pass_osx(account=None, server=None):
     params = {
         'security': '/usr/bin/security',
         'command':  'find-internet-password',
@@ -10,3 +23,9 @@ def get_keychain_pass(account=None, server=None):
     command = "%(security)s %(command)s -g -a %(account)s -s %(server)s" % params
     outtext = commands.getoutput(command)
     return re.match(r'password: "(.*)"', outtext).group(1)
+
+def get_keychain_pass(account=None, server=None):
+    if platform == "darwin":
+        return get_keychain_pass_osx(account=account, server=server)
+    else:
+        return get_keychain_pass_linux(account=account, server=server)
