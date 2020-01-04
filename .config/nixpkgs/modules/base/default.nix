80e6285f19b512b571f5fb81c1f32136a2a8fcc5
Danielle Lancashire <dani@builds.terrible.systems>
2020-01-04 15:40:15 +0100

    gpg-agent: Migrate config to home-manager

--- .config/nixpkgs/modules/base/default.nix
+++ .config/nixpkgs/modules/base/default.nix
@@ -10,4 +10,18 @@
     zsh
     lm_sensors
   ];
+
+  services.gpg-agent = {
+    enable = true;
+
+    # Cache Keys for 30 mins
+    defaultCacheTtl = 1800; # Cache GPG Keys for 30 mins
+    defaultCacheTtlSsh = 1800; # Cache GPG-SSH Keys for 30 mins
+
+    # Require a key entry every two hours even if they've been used recently.
+    maxCacheTtl = 7200;
+    maxCacheTtlSsh = 7200;
+
+    enableSshSupport = true;
+  };
 }
