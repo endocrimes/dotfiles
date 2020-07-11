3bc6b81d92573bfa23c2d112e2e3d15fe4e38717
Danielle Lancashire <dani@builds.terrible.systems>
2020-07-11 21:15:03 +0200

    i3: Updates for newer tools

--- .config/i3/lock.sh
+++ .config/i3/lock.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
-i3lock -fi /etc/background-image.png
+# wget -q --spider http://google.com
+
+image=/etc/background-image.png
+
+# if [ $? -eq 0 ]; then
+#   resolution=$(xrandr | grep \* | cut -d' ' -f4)
+#   url="https://source.unsplash.com/random/$resolution?mountain"
+#   wget -O /tmp/locker.jpg "$url"
+#   sleep 5
+#   image=/tmp/locker.jpg
+# fi
+
+i3lock -fi "$image"
