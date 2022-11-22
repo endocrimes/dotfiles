dd1d43a8cfea9c6a0c228f3269f962d350791ec9
Danielle Lancashire <dani@builds.terrible.systems>
2022-11-22 16:22:44 +0100

    osx: disable most recently used spaces by default

--- .config/os/osx/configure.sh
+++ .config/os/osx/configure.sh
@@ -68,6 +68,9 @@ defaults write com.apple.dock autohide -bool true
 # Set the icon size of Dock items to 45 points
 defaults write com.apple.dock tilesize -int 45
 
+# Disable automatic rearranging of spaces
+defaults write com.apple.dock mru-spaces -bool false
+
 # Restart the dock for changes to take effect
 killall Dock
 
